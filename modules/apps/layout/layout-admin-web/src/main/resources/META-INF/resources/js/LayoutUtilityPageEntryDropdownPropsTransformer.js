4c13ddc0adec399aab7fb8c3100e608b2f083434
Bárbara Cabrera <barbara.cabrera@liferay.com>
2022-10-17 14:59:44 +0200

    LPS-165762 Add Make a Copy Action for the Utility Pages

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/LayoutUtilityPageEntryDropdownPropsTransformer.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/LayoutUtilityPageEntryDropdownPropsTransformer.js
@@ -21,6 +21,11 @@ import {
 import openDeleteLayoutModal from './openDeleteLayoutModal';
 
 const ACTIONS = {
+
+	copyLayoutUtilityPageEntry({copyLayoutUtilityPageEntryURL}) {
+		send(copyLayoutUtilityPageEntryURL);
+	},
+
 	deleteLayoutUtilityPageEntry({
 		deleteLayoutUtilityPageEntryMessage,
 		deleteLayoutUtilityPageEntryURL,
--- modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryLocalServiceImpl.java
+++ modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryLocalServiceImpl.java
@@ -14,6 +14,9 @@
 
 package com.liferay.layout.utility.page.service.impl;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.layout.utility.page.exception.DuplicateLayoutUtilityPageEntryExternalReferenceCodeException;
 import com.liferay.layout.utility.page.exception.LayoutUtilityPageEntryNameException;
 import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
@@ -22,12 +25,15 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.ColorScheme;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutSetLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -36,6 +42,7 @@ import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -100,6 +107,50 @@ public class LayoutUtilityPageEntryLocalServiceImpl
 
 		return layoutUtilityPageEntryPersistence.update(layoutUtilityPageEntry);
 	}
+
+	@Override
+	public LayoutUtilityPageEntry copyLayoutUtilityPageEntry(
+		long userId, long groupId, long layoutUtilityPageEntryId,
+		ServiceContext serviceContext) throws PortalException {
+
+		LayoutUtilityPageEntry sourceLayoutUtilityPageEntry =
+			layoutUtilityPageEntryPersistence.findByPrimaryKey(
+				layoutUtilityPageEntryId);
+
+		String name = _getUniqueCopyName(
+			groupId, sourceLayoutUtilityPageEntry.getName(),
+			sourceLayoutUtilityPageEntry.getType(),
+			serviceContext.getLocale());
+
+		return addLayoutUtilityPageEntry(
+			null, userId, serviceContext.getScopeGroupId(),
+			name, sourceLayoutUtilityPageEntry.getType(),
+			0);
+	}
+
+	private String _getUniqueCopyName(
+		long groupId, String sourceName, int type, Locale locale) {
+
+		String copy = _language.get(locale, "copy");
+
+		String name = StringUtil.appendParentheticalSuffix(sourceName, copy);
+
+		for (int i = 1;; i++) {
+			LayoutUtilityPageEntry layoutUtilityPageEntry =
+				layoutUtilityPageEntryLocalService.fetchByG_N_T(
+					groupId, name, type);
+
+			if (layoutUtilityPageEntry == null) {
+				break;
+			}
+
+			name = StringUtil.appendParentheticalSuffix(
+				sourceName, copy + StringPool.SPACE + i);
+		}
+
+		return name;
+	}
+
 
 	@Override
 	public LayoutUtilityPageEntry fetchDefaultLayoutUtilityPageEntry(
@@ -324,5 +375,14 @@ public class LayoutUtilityPageEntryLocalServiceImpl
 
 	@Reference
 	private UserLocalService _userLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private PortletFileRepository _portletFileRepository;
 
 }
