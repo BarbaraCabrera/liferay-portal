acbf476fb5ff68f1f7f7869c0692a615484d9c7e
Bárbara Cabrera <barbara.cabrera@liferay.com>
2022-10-25 12:20:38 +0200

    LPS-165763 Move associate classes to Layout Impl

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/headless/delivery/dto/v1_0/structure/importer/helper/PortletConfigurationImporterHelper.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/headless/delivery/dto/v1_0/structure/importer/helper/PortletConfigurationImporterHelper.java
@@ -15,8 +15,8 @@
 package com.liferay.layout.internal.headless.delivery.dto.v1_0.structure.importer.helper;
 
 import com.liferay.layout.internal.importer.PortletConfigurationImporterTracker;
-import com.liferay.layout.importer.PortletConfigurationImporter;
-import com.liferay.layout.importer.PortletPreferencesPortletConfigurationImporter;
+import com.liferay.layout.page.template.importer.PortletConfigurationImporter;
+import com.liferay.layout.page.template.importer.PortletPreferencesPortletConfigurationImporter;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
