9910b9ea83501af43589026033f07478a3ee6677
Yurena Cabrera <yurena.cabrera@liferay.com>
2023-08-10 10:20:34 +0100

    LPS-192600 buildService

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/service/LayoutPageTemplateEntryServiceWrapper.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/service/LayoutPageTemplateEntryServiceWrapper.java
@@ -132,6 +132,48 @@ public class LayoutPageTemplateEntryServiceWrapper
 	}
 
 	@Override
+	public java.util.List<Object>
+		getDisplayPageCollectionsAndLayoutPageTemplateEntries(
+			long groupId, int type, int start, int end,
+			com.liferay.portal.kernel.util.OrderByComparator<Object>
+				orderByComparator) {
+
+		return _layoutPageTemplateEntryService.
+			getDisplayPageCollectionsAndLayoutPageTemplateEntries(
+				groupId, type, start, end, orderByComparator);
+	}
+
+	@Override
+	public java.util.List<Object>
+		getDisplayPageCollectionsAndLayoutPageTemplateEntries(
+			long groupId, String name, int type, int start, int end,
+			com.liferay.portal.kernel.util.OrderByComparator<Object>
+				orderByComparator) {
+
+		return _layoutPageTemplateEntryService.
+			getDisplayPageCollectionsAndLayoutPageTemplateEntries(
+				groupId, name, type, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getDisplayPageCollectionsAndLayoutPageTemplateEntriesCount(
+		long groupId, int[] types) {
+
+		return _layoutPageTemplateEntryService.
+			getDisplayPageCollectionsAndLayoutPageTemplateEntriesCount(
+				groupId, types);
+	}
+
+	@Override
+	public int getDisplayPageCollectionsAndLayoutPageTemplateEntriesCount(
+		long groupId, String name, int[] types) {
+
+		return _layoutPageTemplateEntryService.
+			getDisplayPageCollectionsAndLayoutPageTemplateEntriesCount(
+				groupId, name, types);
+	}
+
+	@Override
 	public java.util.List<LayoutPageTemplateEntry> getLayoutPageTemplateEntries(
 		long groupId, int type, int status, int start, int end,
 		com.liferay.portal.kernel.util.OrderByComparator
