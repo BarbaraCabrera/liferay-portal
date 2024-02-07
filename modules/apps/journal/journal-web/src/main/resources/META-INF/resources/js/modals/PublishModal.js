aecb1f5df8ae3f9071956dab6767bd11558b11b6
Victor Galan <victor.galan@liferay.com>
2024-02-07 12:35:16 +0100

    LPD-17233 Show error if the date is empty when saving

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/modals/PublishModal.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/modals/PublishModal.js
@@ -90,14 +90,24 @@ export default function PublishModal({
 							displayType="primary"
 							form={formId}
 							onClick={() => {
-								if (dateError) {
+								if (!displayDate) {
+									setDateError(
+										Liferay.Language.get(
+											'please-enter-a-valid-date'
+										)
+									);
+									setShowErrorAlert(true);
+								}
+								else if (dateError) {
 									setShowErrorAlert(true);
 								}
 								else {
 									onPublishButtonClick();
 								}
 							}}
-							type={!dateError ? 'submit' : 'button'}
+							type={
+								!dateError && displayDate ? 'submit' : 'button'
+							}
 						>
 							{button}
 						</ClayButton>
