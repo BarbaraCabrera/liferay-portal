413a3a181b9dbccb18d35a5147f9ffc5614d0179
Barbara Cabrera <barbara.cabrera@liferay.com>
2024-10-11 13:47:53 +0200

    LPD-35627 Move ActionUtil to the API to reuse it

--- modules/apps/portlet-configuration/portlet-configuration-icon/portlet-configuration-icon-api/src/main/java/com/liferay/portlet/configuration/icon/action/ActionUtil.java
+++ modules/apps/portlet-configuration/portlet-configuration-icon/portlet-configuration-icon-api/src/main/java/com/liferay/portlet/configuration/icon/action/ActionUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portlet.configuration.web.internal.portlet.action;
+package com.liferay.portlet.configuration.icon.action;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
